25472de4b5cb82bad29fe5802b70299164e86b64
SouravKumarBarman <souravkumarbarman1729@gmail.com>
2025-09-23 17:12:58 +0530

    clean the moving cursor

--- time.c
+++ time.c
@@ -1,23 +1,40 @@
-#include<stdio.h> 
-#include<time.h>
-#include<unistd.h>
-int main(){
-  printf("Current Time\n");
-  while(1){
-    time_t currentTime;
-    time(&currentTime);
-    struct tm *t=localtime(&currentTime);
-    int hour=t->tm_hour;
-    if(hour>12){
-      hour=hour-12;
-    }
-    int min=t->tm_min;
-    int sec=t->tm_sec;
-    printf("%02d:",hour);
-    printf("%02d:",min);
-    printf("%02d\r",sec);
-    usleep(10000);
-  }
-  printf("showing your cursor\n");
-  return 0;
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+
+static volatile int keep_running = 1;
+
+void show_cursor() {
+    printf("\e[?25h");
+    fflush(stdout);
 }
+
+void signal_handler(int sig) {
+    printf("\nCaught signal %d (Ctrl+C). Restoring cursor and exiting.\n", sig);
+    keep_running = 0; 
+}
+
+int main() {
+    printf("\e[?25l");
+    signal(SIGINT, signal_handler);
+    printf("Current Time\n");
+    while (keep_running) {
+        time_t currentTime;
+        time(&currentTime);
+        struct tm *t = localtime(&currentTime);
+        int hour = t->tm_hour;
+        if (hour > 12) {
+            hour = hour - 12;
+        }
+        int min = t->tm_min;
+        int sec = t->tm_sec;
+        printf("%02d:%02d:%02d\r", hour, min, sec);
+	fflush(stdout);
+        usleep(10000);
+    }
+    show_cursor();
+    return 0;
+}
+
